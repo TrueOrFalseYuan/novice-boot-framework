43463eafe48dfaaad4628212e7c673cd8465f1bb
trueorfalse.yuan <trueorfalse.yuan@gmail.com>
2018-12-17 20:48:56 +0800

    add common regex for validation

--- src/main/java/cn/kinkii/novice/framework/controller/GlobalRegex.java
+++ src/main/java/cn/kinkii/novice/framework/controller/GlobalRegex.java
@@ -3,5 +3,6 @@ package cn.kinkii.novice.framework.controller;
 public class GlobalRegex {
 
     public static final String REGEX_MOBILE = "^1\\d{10}$";
+    public static final String REGEX_PASSWORD = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$";
 
 }
