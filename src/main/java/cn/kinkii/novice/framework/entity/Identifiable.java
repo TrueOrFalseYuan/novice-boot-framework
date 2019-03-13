f0ccaa4f180833dfe6bd52ed65420b1cedbd785e
trueorfalse.yuan <trueorfalse.yuan@gmail.com>
2019-03-13 18:29:12 +0800

    make all entity serializable

--- src/main/java/cn/kinkii/novice/framework/entity/Identifiable.java
+++ src/main/java/cn/kinkii/novice/framework/entity/Identifiable.java
@@ -8,9 +8,10 @@ import java.io.Serializable;
  * @param <ID> 主键类型
  * @author TrueOrFalse.Yuan
  */
-public interface Identifiable<ID extends Serializable> {
+public interface Identifiable<ID extends Serializable> extends Serializable {
 
-  ID getId();
+    ID getId();
 
-  void setId(ID id);
+    void setId(ID id);
+
 }
