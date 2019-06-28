ce6444375cb17e0980a5308628025f69b64eea2d
lleid <liulei5913@126.com>
2019-06-28 13:48:04 +0800

    添加apo防止重复提交数据

---
+++ src/main/java/cn/kinkii/novice/framework/aop/NoRepeatSubmit.java
@@ -0,0 +1,13 @@
+package cn.kinkii.novice.framework.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface NoRepeatSubmit {
+
+    int lockTime() default 10;
+}
