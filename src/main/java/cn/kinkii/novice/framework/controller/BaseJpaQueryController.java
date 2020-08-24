e156e9156aac6c09b5d00c6a782f1894ba2b727d
trueorfalse.yuan <shittc76ywh@126.com>
2020-08-24 17:48:18 +0800

    update handleQuery/handelPageQuery & update framework version

--- src/main/java/cn/kinkii/novice/framework/controller/BaseJpaQueryController.java
+++ src/main/java/cn/kinkii/novice/framework/controller/BaseJpaQueryController.java
@@ -4,6 +4,7 @@ import cn.kinkii.novice.framework.controller.query.jpa.JpaQuery;
 import cn.kinkii.novice.framework.controller.query.jpa.JpaQuerySpecification;
 import cn.kinkii.novice.framework.entity.Identifiable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,8 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.io.Serializable;
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @SuppressWarnings({"unchecked", "WeakerAccess"})
@@ -25,7 +28,8 @@ public abstract class BaseJpaQueryController<E extends Identifiable<ID>, ID exte
     }
 
     @SuppressWarnings("unused")
-    protected void handleQuery(Q query, Principal principal) {
+    protected Boolean handleQuery(Q query, Principal principal) {
+        return true;
     }
 
     protected Boolean canQueryByPage() {
@@ -33,8 +37,8 @@ public abstract class BaseJpaQueryController<E extends Identifiable<ID>, ID exte
     }
 
     @SuppressWarnings("unused")
-    protected void handlePageQuery(Q query, Pageable pageable, Principal principal) {
-        handleQuery(query, principal);
+    protected Boolean handlePageQuery(Q query, Pageable pageable, Principal principal) {
+        return handleQuery(query, principal);
     }
 
     @RequestMapping(value = "/query", method = {RequestMethod.POST, RequestMethod.GET})
@@ -44,8 +48,11 @@ public abstract class BaseJpaQueryController<E extends Identifiable<ID>, ID exte
         if (!canQuery()) {
             return null;
         }
-        handleQuery(query, principal);
-        return (List<E>) invoke(getRepository(), "findAll", new Class[]{Specification.class}, List.class, new JpaQuerySpecification<>(query));
+        if (handleQuery(query, principal)) {
+            return (List<E>) invoke(getRepository(), "findAll", new Class[]{Specification.class}, List.class, new JpaQuerySpecification<>(query));
+        } else {
+            return Collections.emptyList();
+        }
     }
 
     @RequestMapping(value = "/query/page", method = {RequestMethod.POST, RequestMethod.GET})
@@ -55,11 +62,14 @@ public abstract class BaseJpaQueryController<E extends Identifiable<ID>, ID exte
         if (!canQueryByPage()) {
             return null;
         }
-        handlePageQuery(query, pageable, principal);
-        if (pageable.getSort().iterator().hasNext()) {
-            query.setIsSortByAnnotation(false);
+        if (handlePageQuery(query, pageable, principal)) {
+            if (pageable.getSort().iterator().hasNext()) {
+                query.setIsSortByAnnotation(false);
+            }
+            return (Page<E>) invoke(getRepository(), "findAll", new Class[]{Specification.class, Pageable.class}, Page.class, new Object[]{new JpaQuerySpecification<>(query), pageable});
+        } else {
+            return new PageImpl<>(Collections.emptyList(), pageable, 0);
         }
-        return (Page<E>) invoke(getRepository(), "findAll", new Class[]{Specification.class, Pageable.class}, Page.class, new Object[]{new JpaQuerySpecification<>(query), pageable});
     }
 
     @RequestMapping(value = "/count", method = {RequestMethod.POST, RequestMethod.GET})
