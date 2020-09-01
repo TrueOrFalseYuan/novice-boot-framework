dab9e3df8416dd6d730621b161f6bb2cfd9ef9f5
trueorfalse.yuan <shittc76ywh@126.com>
2020-09-01 18:46:26 +0800

    add handleQueryAllByPage

--- src/main/java/cn/kinkii/novice/framework/controller/BaseModelQueryController.java
+++ src/main/java/cn/kinkii/novice/framework/controller/BaseModelQueryController.java
@@ -3,6 +3,7 @@ package cn.kinkii.novice.framework.controller;
 import cn.kinkii.novice.framework.controller.exception.InternalServiceException;
 import cn.kinkii.novice.framework.entity.Identifiable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,10 +26,6 @@ public abstract class BaseModelQueryController<E extends Identifiable<ID>, ID ex
         return true;
     }
 
-    protected Boolean canQueryAll() {
-        return true;
-    }
-
     protected Boolean handleGet(ID id, Principal principal) {
         // Do nothing...
         return true;
@@ -38,9 +35,18 @@ public abstract class BaseModelQueryController<E extends Identifiable<ID>, ID ex
         // Do nothing...
     }
 
+    protected Boolean canQueryAll() {
+        return true;
+    }
+
     protected Boolean handleQueryAll(Principal principal) {
         // Do nothing...
         return true;
+    }
+
+    protected Boolean handleQueryAllByPage(Pageable pageable, Principal principal) {
+        // Do nothing...
+        return handleQueryAll(principal);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
@@ -89,8 +95,10 @@ public abstract class BaseModelQueryController<E extends Identifiable<ID>, ID ex
             return null;
         }
         try {
-            handleQueryAll(principal);
-            return (Page<E>) invokeMethods("findAll", new Class[]{Pageable.class}, Page.class, pageable);
+            if (handleQueryAllByPage(pageable, principal)) {
+                return (Page<E>) invokeMethods("findAll", new Class[]{Pageable.class}, Page.class, pageable);
+            }
+            return new PageImpl<>(Collections.emptyList(), pageable, 0);
         } catch (RuntimeException ignored) {
             throw new InternalServiceException(getMessage(GlobalMessage.ERROR_SERVICE.getMessageKey()));
         }
